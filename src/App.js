ceb9e08cb48ad81af1ec5eeebe25a02874ece850
Facundo Conde <facundo.conde92@gmail.com>
2021-06-09 22:56:11 -0300

    agregando NavBar en carpeta components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import NavBar from './components/NavBar'
 
 class App extends React.Component {
   render() {
     return(
-      <nav>
-        <ul className="main-nav">
-          <li>opción1</li>
-          <li>opcion2</li>
-          <li>opcion3</li>
-          <li>opcion4</li>
-        </ul>
-      </nav>
+      <NavBar/>
+
     )
   }
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class navBar extends Component {
+    render() {
+        return (
+            <>
+            <h1>Design Dimensions</h1>
+            <nav className='main-nav'>
+                <p>Opcion1</p>
+                <p>Opcion2</p>
+                <p>Opcion3</p>
+            </nav>
+            </>
+        )
+    }
+}
