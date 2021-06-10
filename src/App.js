f4807737321d7a6be23e80921239a284af19c222
Facundo Conde <facundo.conde92@gmail.com>
2021-06-10 19:54:15 -0300

    entrega clase 4 - Conde

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import NavBar from './components/NavBar.js'
 import './App.css';
-import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer.js';
 
 class App extends React.Component {
   render() {
     return(
+    <>
       <NavBar/>
-
+      <ItemListContainer title='Bienvenidos a Design Dimensons!' className='nav-principal'/>
+    </>
     )
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import CartWidget from './CartWidget'
 
 export default class navBar extends Component {
     render() {
         return (
             <>
+            <link
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+                integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+                crossOrigin="anonymous"
+            />
             <h1>Design Dimensions</h1>
-            <nav className='main-nav'>
-                <p>Opcion1</p>
-                <p>Opcion2</p>
-                <p>Opcion3</p>
+            <nav className='nav-principal'>
+                <a className='boton' href='.'>Opcion1</a>
+                <a className='boton' href='.'>Opcion2</a>
+                <a className='boton' href='.'>Opcion3</a>
+                <CartWidget/>
             </nav>
             </>
         )
