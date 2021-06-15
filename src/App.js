1b2d333bc81973a871c5fd07814334ccdbc3dea3
Facundo Conde <facundo.conde92@gmail.com>
2021-06-15 19:37:03 -0300

    delete local pics

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends React.Component {
       <NavBar/>
       <div className='container'>
         <div className='cardCentrada'>
-          <UserCard photo='./images/falloutUser.png' name='Kid' whenJoined='April 25' description='Love fallouts' friendsCount='1'/>
+          <UserCard photo='https://www.freepnglogos.com/uploads/fallout-4-logo/allrez-fallout-4-fallout-mods-8.png' name='Kid' whenJoined='April 25' description='Love fallouts' friendsCount='1'/>
         </div>
         <div className='cardCentrada'>
-          <UserCard photo='./images/DonkeyKongUser.jpg' name='Donkey' whenJoined='december 19' description='Love bananas' friendsCount='33545'/>
+          <UserCard photo='https://nintendo.pe/wp-content/uploads/2016/12/Donkey_Kong_Profile_Artwork.jpg' name='Donkey' whenJoined='december 19' description='Love bananas' friendsCount='33545'/>
         </div>
         <div className='cardCentrada'>
           <UserCard photo='https://thispersondoesnotexist.com/image' name='Random' whenJoined='some day' description='Who knows' friendsCount='∞'/>
