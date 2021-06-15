6ccb38afe75f6bea115ba64e154690f8805801d0
Facundo Conde <facundo.conde92@gmail.com>
2021-06-15 19:23:09 -0300

    friendsCount prop

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ class App extends React.Component {
       <NavBar/>
       <div className='container'>
         <div className='cardCentrada'>
-          <UserCard photo='./images/falloutUser.png' name='Kid' whenJoined='April 25' description='Love fallouts'/>
+          <UserCard photo='./images/falloutUser.png' name='Kid' whenJoined='April 25' description='Love fallouts' friendsCount='1'/>
         </div>
         <div className='cardCentrada'>
-          <UserCard photo='./images/DonkeyKongUser.jpg' name='Donkey' whenJoined='december 19' description='Love bananas'/>
+          <UserCard photo='./images/DonkeyKongUser.jpg' name='Donkey' whenJoined='december 19' description='Love bananas' friendsCount='33545'/>
         </div>
         <div className='cardCentrada'>
-          <UserCard photo='https://thispersondoesnotexist.com/image' name='Random' whenJoined='some day' description='Who knows'/>
+          <UserCard photo='https://thispersondoesnotexist.com/image' name='Random' whenJoined='some day' description='Who knows' friendsCount='∞'/>
         </div>
       </div>
 
