855212a115f84778cded85d012668201a643490b
Facundo Conde <facundo.conde92@gmail.com>
2021-06-22 19:38:22 -0300

    add headder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import NavBar from "./components/NavBar/NavBar.js";
+import Header from "./components/Header/Header.js";
 import ItemCount from "./components/ItemCount/ItemCount.js";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer.js";
 
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <>
-        <NavBar />
+        <Header />
         <ItemDetailContainer />
         <ItemCount />
       </>
